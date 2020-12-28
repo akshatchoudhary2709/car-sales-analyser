0b965741447fc1dc3e37623cff2c6527ad4d38de
Akshat Choudhary <48729767+akshatchoudhary2709@users.noreply.github.com>
2020-12-28 14:04:06 +0530

    Update settings.py

--- analyser/settings.py
+++ analyser/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -126,3 +126,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+#activate django_heroku
+django_heroku.settings(locals())
